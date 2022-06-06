adbe439c3349e6ab3cd5b9004415d95e8f30fbdb
Ross Rosales <ross.erosales@gmail.com>
2022-06-05 23:51:31 -0500

    working app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,73 @@
 // This file has the bulk of the code. It contains major functions, variables, local storage. 
 
-import React, { useState, useRef, useEffect} from 'react';
-import NameOrigin from './NameOrigin';
+import React, { useState, useRef } from 'react';
+//import { NameOrigin } from './NameOrigin';
+//import { nametobesearched } from './nametobesearched';
 
 
 
 function App() {
+  const [curname, setName] = useState(''); //[first, second]: first is current state, second function that allows up to update current state
   const nameInput = useRef() // Uses imported package
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-      'X-RapidAPI-Key': 'cd4262cdfemshad95db473709b99p1bae6bjsn69100feb95ec'
-    }
-  };
+  const [icon, seticon] = useState()
+  const [condition, setCondition] = useState()
+  const [temp_f, setTempf] = useState()
+  const [feelslike_f, setFeelslikef] = useState()
+  const [gust_mph, setGustmph] = useState()
+  const [localtime, setlocaltime] = useState()
   
-  fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=Brooklyn', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err))
+  
 
   function SearchName() {
     const name = nameInput.current.value
+    setName(name)
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        'X-RapidAPI-Key': 'cd4262cdfemshad95db473709b99p1bae6bjsn69100feb95ec'
+      }
+    };
+    
+    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=' + name, options)
+    .then((data) => {
+      return data.json();
+    }).then((completedata) => {
+        console.log(completedata)
+        seticon(completedata.current.condition.icon)
+        setCondition(completedata.current.condition.text)
+        setTempf(completedata.current.temp_f)
+        setFeelslikef(completedata.current.feelslike_f)
+        setGustmph(completedata.current.gust_mph)
+        setlocaltime(completedata.location.localtime)
+
+        
+      });
+    
+
     if (name === '') return /// '===' is used to match both value and data type, '==' matches only value
     console.log(name) //in console.log (inspect webpage)
     nameInput.current.value=null //clears text box
   }
-
   return (
     <>
-    <NameOrigin/> 
-    <div>Name Origin</div>
+    <h1>Weather</h1>
     <input ref={nameInput} type="text"/>
-    <button onClick={SearchName}>Search Name</button>
-    <div>{}</div>
+    <button onClick={SearchName}>Search</button>
+    <p></p>
+
+    <img src={icon} alt="icon" />
+    <p><b>Weather in: </b>{curname}</p>
+    
+    
+    <p><b>Condition: </b>{condition} </p>
+    <p><b>Current Temp: </b>{temp_f}<span>&#176;</span>F</p>
+    <p><b>Feels Like: </b>{feelslike_f}<span>&#176;</span>F</p>
+    <p><b>Winds: </b>{gust_mph}mph</p>
+    <p><b>Date/Time: </b>{localtime}</p>
     </>
   )
 }
 
-export default App;
+export default App; // this file is exported to index.js, in index.js this file is imported
--- src/NameOrigin.js
+++ src/NameOrigin.js
@@ -1,7 +1,26 @@
-// This file is used for
+// This file is used for API
+import App, { hello } from './App';
 
-import React from 'react'
+async function NameOrigin() {
+ // var temp = null;
 
-export default function NameOrigin() {
-  return null
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+      'X-RapidAPI-Key': 'cd4262cdfemshad95db473709b99p1bae6bjsn69100feb95ec'
     }
+  };
+  
+  const response = await fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=' + {hello}, options);
+
+  //const data = response.text();
+  console.log(response);
+//    .then(response => temp = response.json()) // When data is retrieved, method 'then' handles the argument response
+//    .then(response => console.log(response))
+//    .catch(err => console.error(err))
+//
+  return console.log(response)
+}
+
+export {NameOrigin};
